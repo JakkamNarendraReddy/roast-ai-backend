0fb25ae8f54826a5774c6b541d1492428195eabe
JakkamNarendraReddy <153424899+JakkamNarendraReddy@users.noreply.github.com>
2026-04-17 21:54:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def roast():
         data = request.json
         prompt = data["prompt"]
 
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key={API_KEY}"
 
         response = requests.post(
             url,
